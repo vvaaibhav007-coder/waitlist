5aacf3e171234610233c5c0d0dc71f1739c0a7ce
vvaaibhav007-coder <vvaaibhav.007@gmail.com>
2026-04-21 16:30:51 +0530

    download page

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  appType: "spa",
   base: "/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
